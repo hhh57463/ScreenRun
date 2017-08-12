9c31393c9e41224cb0449e36533715030e461041
Huduck <hhh57463@naver.com>
2017-08-12 15:57:07 +0900

    Item Zen

---
+++ Assets/2_GameScene/1_Script/Item.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public MapScroll MapSc;
+
+    // Use this for initialization
+    void Start()
+    {
+        MapSc = GameObject.Find("Map").GetComponent<MapScroll>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (MapSc.nMapNum)
+        {
+            case 0:
+                transform.Translate(Vector2.down * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 1:
+                transform.Translate(Vector2.left * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 2:
+                transform.Translate(Vector2.right * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 3:
+                transform.Translate(Vector2.up * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 4:
+                transform.Translate(Vector2.left * SGameMng.I.fItemSpeed * Time.deltaTime);
+                transform.Translate(Vector2.down * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 5:
+                transform.Translate(Vector2.right * SGameMng.I.fItemSpeed * Time.deltaTime);
+                transform.Translate(Vector2.up * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 6:
+                transform.Translate(Vector2.left * SGameMng.I.fItemSpeed * Time.deltaTime);
+                transform.Translate(Vector2.up * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+            case 7:
+                transform.Translate(Vector2.right * SGameMng.I.fItemSpeed * Time.deltaTime);
+                transform.Translate(Vector2.down * SGameMng.I.fItemSpeed * Time.deltaTime);
+                break;
+
+        }
+
+        if (transform.localPosition.x <= -12f)
+        {
+            SGameMng.I.bItemZenStart = true;
+            Destroy(gameObject);
+        }
+
+        if (transform.localPosition.x >= 12f)
+        {
+            SGameMng.I.bItemZenStart = true;
+            Destroy(gameObject);
+        }
+
+        if (transform.localPosition.y <= -7f)
+        {
+            SGameMng.I.bItemZenStart = true;
+            Destroy(gameObject);
+        }
+
+        if (transform.localPosition.y >= 7f)
+        {
+            SGameMng.I.bItemZenStart = true;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Hero"))
+        {
+            //StartCoroutine(SGameMng.I.ItemZen());
+            SGameMng.I.bItemZenStart = true;
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/2_GameScene/1_Script/SGameMng.cs
+++ Assets/2_GameScene/1_Script/SGameMng.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SGameMng : MonoBehaviour {
+public class SGameMng : MonoBehaviour
+{
 
     private static SGameMng _Instance = null;
+
+    public GameObject ItemPre;
+    public Transform ItemZenPr;
+
+    public Vector3 ItemPosVec;
+
+    public bool bItemZenStart = false;
+
     public float fFireDownSpeed = 2f;
     public float fFireSpeed = 0.5f;
     public float fMapScrollSpeed = 0.5f;
     public float fHandSpeed = 10.0f;
+    public float fItemSpeed = 2f;
     public float fHeroCompulsionMoveSpeed = 0.5f;
     public float fHeroSpeed = 4f;
     public float fDifficultyUpTime = 10f;
+    public float fIteZenTime = 10f;
+    public float[] fZenPos;
+
 
     public static SGameMng I
     {
@@ -28,6 +41,30 @@ public class SGameMng : MonoBehaviour {
     void Awake()
     {
         _Instance = this;
+        StartCoroutine(ItemZen());
+    }
+
+    void Update()
+    {
+        if (bItemZenStart)
+        {
+            StartCoroutine(ItemZen());
+            bItemZenStart = false;
+        }
+    }
+
+    public IEnumerator ItemZen()
+    {
+        ItemPosRandomSetting();
+        yield return new WaitForSeconds(fIteZenTime);
+        Instantiate(ItemPre, ItemPosVec, Quaternion.identity, ItemZenPr);
+    }
+
+    void ItemPosRandomSetting()
+    {
+        fZenPos[0] = Random.Range(-6f, 6f);
+        fZenPos[1] = Random.Range(-4f, 4f);
+        ItemPosVec = new Vector3(fZenPos[0], fZenPos[1], 0f);
     }
 
 }
